22de15422cd9501a8197804ba8c45d7b9f76c406
vincent178 <vincent.007.cn@gmail.com>
2018-02-17 10:56:35 +0800

    update

--- server.go
+++ server.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "IP: %s", c.Request.RemoteAddr)
+		ip := c.Request.RemoteAddr
+		fmt.Printf("IP: %s", ip)
+		c.String(200, "IP: %s", ip)
 	})
 	r.Run(":9000")
 }
